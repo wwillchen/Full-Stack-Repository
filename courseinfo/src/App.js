5ca3a0ae6a2f0a460d2d40468240dcee5c45a4ba
wwillchen <107781106+wwillchen@users.noreply.github.com>
2022-12-24 17:32:08 -0500

    courseinfo completed

--- courseinfo/src/App.js
+++ courseinfo/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+const App = () => {
+  const name = 'Half Stack application development'
+  const course = [
+      { name : 'Fundamentals of React', exercises : 10 },
+      { name : 'Using props to pass data', exercises : 7 },
+      { name : 'State of a component', exercises : 14 }
+    ]
 
-function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={name}/>
+      <Content course={course}/>
+      <Total exercise1={course[0].exercises} exercise2={course[1].exercises} exercise3={course[2].exercises}/>
     </div>
-  );
+  )
+}
+
+const Header = (props) => {
+  return (
+  <div>
+    <h1>{props.course}</h1>
+  </div>
+  )
+}
+
+const Content = ({course}) => {
+  return (
+  <div>
+    <Part course={course[0]}/>
+    <Part course={course[1]}/>
+    <Part course={course[2]}/>
+  </div>
+  )
+}
+
+const Part = ({course}) => (
+  <div>
+    <p>{course.name} {course.exercises}</p>
+  </div>
+)
+const Total = (props) => {
+  return (
+  <div>
+    <p>Number of exercises {props.exercise1 + props.exercise2 + props.exercise3}</p>
+  </div>
+  )
 }
 
 export default App;

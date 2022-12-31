f27a633d5a37c1ae213dd57982b25485643319c4
wwillchen <107781106+wwillchen@users.noreply.github.com>
2022-12-31 16:44:34 -0500

    exercises 1.6-1.10

--- unicafe/src/App.js
+++ unicafe/src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>give feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} total={good + neutral + bad}/>
+      <p></p> 
     </div>
-  );
+
+  )
 }
+
+const Statistics = ({good, neutral, bad, total}) => {
+  if (total == 0) {
+    return (
+    <div>
+      <p>No feedback given</p>
+    </div>
+    )
+  }
+    return (
+      <div>
+        <StatisicLine text='good' value={good}/>
+        <StatisicLine text='neutral' value={neutral}/>
+        <StatisicLine text='bad' value={bad}/>
+        <StatisicLine text='all' value={total}/>
+        <StatisicLine text='average' value={total / 3}/>
+        <StatisicLine text='positive' value={good / total + ' %'}/>
+      </div>
+    )
+}
+
+const StatisicLine = ({text, value}) => <div>{text} {value}</div>
 
 export default App;
